4f69fe8574414965050d82216e95bc4f613f9039
jppeanuts <131636978+jppeanuts@users.noreply.github.com>
2024-03-26 07:09:48 +0900

    Add files via upload

---
+++ shared.h
@@ -0,0 +1,31 @@
+#ifndef _SHARED_H_
+#define _SHARED_H_
+
+
+#include <stdio.h>
+#include <math.h>
+#include <zlib.h>
+
+#include "types.h"
+#include "macros.h"
+#include "m68k.h"
+#include "z80.h"
+#include "genesis.h"
+#include "vdp.h"
+#include "render.h"
+#include "mem68k.h"
+#include "memz80.h"
+#include "membnk.h"
+#include "memvdp.h"
+#include "system.h"
+#include "unzip.h"
+#include "fileio.h"
+#include "loadrom.h"
+#include "io.h"
+#include "sound.h"
+#include "fm.h"
+#include "sn76496.h"
+#include "osd.h"
+
+#endif /* _SHARED_H_ */
+
